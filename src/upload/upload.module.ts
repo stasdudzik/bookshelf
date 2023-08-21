220ef0495d9b657f2102f83846b8bead976374bf
Stas Dudzik <stas.dudzik@gmail.com>
2023-08-21 13:05:19 +0200

    use config for throttler params

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -3,12 +3,16 @@ import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 3,
+    ThrottlerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        ttl: configService.getOrThrow('THROTTLE_TTL'),
+        limit: configService.getOrThrow('THROTTLE_LIMIT'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [UploadController],
