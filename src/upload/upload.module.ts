8c745ab4d1b80e6d3c3e4a7bee78580feb0095d4
Stas Dudzik <stas.dudzik@gmail.com>
2023-08-21 12:43:21 +0200

    add throttle guard for rate limiting

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 3,
+    }),
+  ],
   controllers: [UploadController],
-  providers: [UploadService],
+  providers: [
+    UploadService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class UploadModule {}
